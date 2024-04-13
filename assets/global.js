f28f841083573991afaf9012199141271ee9aac1
Lea Verou <lea@verou.me>
2024-04-13 00:04:03 -0400

    oops

--- assets/global.js
+++ assets/global.js
@@ -3,7 +3,7 @@ import "./index.js";
 import "https://prismjs.com/prism.js";
 import renderDemos from "./wc-demos.js";
 
-if (!container.documentElement.matches(".no-home-link")) {
+if (!document.documentElement.matches(".no-home-link")) {
 	let h1 = document.querySelector("h1");
 
 	if (h1 && !h1.parentNode.querySelector(".home")) {
