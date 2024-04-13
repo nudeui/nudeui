966a5368c2e5a2ae4870e9c68e71bc2a0f07e3bc
Lea Verou <lea@verou.me>
2024-04-12 23:32:15 -0400

    Fix URLs

--- assets/index.js
+++ assets/index.js
@@ -7,11 +7,11 @@ let elements = [
 	"nd-calendar",
 ];
 
-await Promise.all(elements.map(id => import(`./${id}/${id}.js`)));
+await Promise.all(elements.map(id => import(`../${id}/${id}.js`)));
 
 // CSS only modules
 let modules = [
 	"nd-switch",
 ];
 
-document.head.insertAdjacentHTML("beforeend", modules.map(id => `<link rel="stylesheet" href="${new URL(`./${id}/${id}.css`, import.meta.url)}" />`).join("\n"));
+document.head.insertAdjacentHTML("beforeend", modules.map(id => `<link rel="stylesheet" href="${new URL(`../${id}/${id}.css`, import.meta.url)}" />`).join("\n"));
