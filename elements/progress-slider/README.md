590dbf63bff8404d98836c750c8288a65082705b
Lea Verou <lea@verou.me>
2024-05-02 01:34:35 -0400

    <progress-slider>

---
+++ elements/progress-slider/README.md
@@ -0,0 +1,86 @@
+---
+id: progress-slider
+---
+<script type="module" src="https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.15.0/cdn/components/format-number/format-number.js"></script>
+<header>
+
+# `<progress-slider>`
+
+Several improvements over the native `<input type=range>`.
+
+</header>
+
+<main>
+
+## Features
+
+- Editable spinner that shows the value or progress of the slider, displayed as a tooltip or inline
+- Convenience methods
+
+## Examples
+
+### Basic
+
+Simplest version:
+
+```html
+<progress-slider></progress-slider>
+```
+
+You can also *provide* a specific slider element yourself so it can be easier to style or customize:
+
+```html
+<progress-slider>
+	<input type=range>
+</progress-slider>
+```
+
+You can also provide a specific element for the value:
+
+```html
+<progress-slider>
+	<sl-format-number slot="value" type="currency" currency="USD"></sl-format-number>
+</progress-slider>
+```
+
+If it has a `value` property it will be assumed to be editable, otherwise it will be read-only.
+
+---
+
+All usual slider attributes work and are simply copied to the slider and spinner elements:
+
+```html
+<progress-slider min="-180" max="180" step="0.01"></progress-slider>
+```
+
+You are encouraged to provide a slider with the right attributes from the start, to minimize updates.
+
+---
+
+By default the number is shown as a read/write tooltip, but you can make it display inline:
+
+```html
+<progress-slider style="--value-position: end"></progress-slider>
+```
+
+Yes, this is a regular CSS property that you can even set in your stylesheet.
+It requires [style queries](https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_containment/Container_size_and_style_queries#container_style_queries_2),
+which means [at the time of this writing, it won’t work in Firefox](https://caniuse.com/css-container-queries-style).
+
+By default shown is the slider value, but you can switch to showing (and editing) the progress instead:
+
+```html
+<progress-slider show="progress"></progress-slider>
+```
+
+## Properties and methods
+
+In addition to the usual properties and methods of a slider (`min`, `max`, `step`, `value`, `defaultValue`), the following are available:
+- `show`: Whether to show the value or the progress. Possible values: `value`, `progress`. Default: `value`.
+- `valueElement`: The element that shows the value.
+- `sliderElement`: The element that is the slider.
+- `progress`: The progress of the slider, as a number between 0 and 1.
+- `progressAt(value)`: Returns the progress at a given point, as a number between 0 and 1.
+- `valueAt(progress)`: Returns the value at a given progress, as a number between `min` and `max`.
+
+</main>
---
+++ elements/progress-slider/progress-slider.js
@@ -0,0 +1,162 @@
+let self = class ProgressSlider extends HTMLElement {
+	sliderElement = null;
+	valueElement = null;
+	#slots = {};
+
+	static tagName = "progress-slider";
+	static observedAttributes = ["min", "max", "step", "value", "show"];
+
+	constructor () {
+		super();
+
+		this.attachShadow({mode: "open"});
+		let styleURL = new URL(self.tagName + ".css", import.meta.url);
+		this.shadowRoot.innerHTML = `
+			<style>@import url("${ styleURL }")</style>
+			<slot>
+				<input type="range">
+			</slot>
+			<slot name="value">
+				<input type="number">
+			</slot>
+		`;
+
+		this.#slots = Object.fromEntries([...this.shadowRoot.querySelectorAll("slot")].map(slot => [slot.name || "slider", slot]));
+
+		this.#slots.slider.addEventListener("slotchange", this);
+		this.#slots.value.addEventListener("slotchange", this);
+
+		this.handleEvent({type: "slotchange"});
+	}
+
+	handleEvent (event) {
+		if (event.type === "slotchange") {
+			let source = "slider";
+
+			for (let name in this.#slots) {
+				let slot = this.#slots[name];
+				let elementProp = name + "Element";
+				let oldElement = this[elementProp];
+				let nodes = slot.assignedNodes();
+				let elements = slot.assignedElements();
+
+				if (name === "slider" && elements.length === 0 && nodes.every(node => !node.nodeValue.trim())) {
+					// Literally every node assigned to this slot is an empty text node. Likely formatting, remove it.
+					// See https://twitter.com/LeaVerou/status/1785904086929346957
+					nodes.forEach(node => node.remove());
+				}
+
+				let element = slot.assignedElements()[0];
+
+				if (name !== source && element) {
+					source = name;
+				}
+
+				element ??= slot.firstElementChild;
+				this[elementProp] = element;
+
+				if (oldElement !== this[elementProp]) {
+					oldElement?.removeEventListener("input", this);
+					this[elementProp]?.addEventListener("input", this);
+				}
+			}
+
+			this[source + "Element"].dispatchEvent(new Event("input", {bubbles: true}));
+		}
+		else if (event.type === "input") {
+			if (event.target === this.sliderElement) {
+				this.#valueChanged({source: "slider"});
+			}
+			else if (event.target === this.valueElement) {
+				this.#valueChanged({source: "value"});
+			}
+
+			this.dispatchEvent(new Event("input", {
+				bubbles: true,
+				originalTarget: event.target,
+			}));
+		}
+	}
+
+	#valueChanged ( {source} ) {
+		let value = this[source + "Element"].value;
+
+		if (source === "slider") {
+			this.valueElement.value = value;
+		}
+		else if (source === "value") {
+			this.sliderElement.value = value;
+		}
+
+		this.style.setProperty("--value", this.value);
+		this.style.setProperty("--progress", this.progress);
+
+		if (!CSS.supports("field-sizing", "content")) {
+			let valueStr = this.value + "";
+			this.valueElement.style.setProperty("--value-length", valueStr.length);
+		}
+	}
+
+	get show () {
+		return this.getAttribute("show");
+	}
+
+	set show (value) {
+		this.setAttribute("show", value);
+	}
+
+	get progress () {
+		return this.progressAt(this.value);
+	}
+
+	progressAt (value) {
+		return (value - this.min) / (this.max - this.min);
+	}
+
+	valueAt (progress) {
+		return this.min + progress * (this.max - this.min);
+	}
+
+	attributeChangedCallback (name, oldValue, newValue) {
+		if (oldValue === newValue) {
+			return;
+		}
+
+		if (name === "show") {
+
+		}
+		else {
+			this.sliderElement.setAttribute(name, newValue);
+			this.valueElement.setAttribute(name, newValue);
+		}
+	}
+}
+
+let defaults = {
+	min: 0,
+	max: 100,
+	step: 1,
+	value: 50,
+	defaultValue: 50,
+}
+
+for (let prop of Object.keys(defaults)) {
+	Object.defineProperty(self.prototype, prop, {
+		get () {
+			let value = this.sliderElement[prop]
+			return value === "" ? defaults[prop] : Number(value);
+		},
+		set (value) {
+			let oldValue = this.sliderElement[prop];
+
+			if (oldValue !== value) {
+				this.sliderElement[prop] = value;
+				this.valueElement[prop] = value;
+			}
+		},
+	});
+}
+
+customElements.define(self.tagName, self);
+
+export default self;
